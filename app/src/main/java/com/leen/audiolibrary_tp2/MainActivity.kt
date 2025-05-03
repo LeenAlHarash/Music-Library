05b1c681a3982e17c20cfedce0ec3e9cafc80152
Leen <lal@dept-crosemont.qc.ca>
2025-05-03 13:02:07 -0400

    #1 : Création de la page profile et ses fonctionnalités

--- app/src/main/java/com/leen/audiolibrary_tp2/MainActivity.kt
+++ app/src/main/java/com/leen/audiolibrary_tp2/MainActivity.kt
@@ -1,20 +1,39 @@
 package com.leen.audiolibrary_tp2
 
+import android.content.Context
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
+private const val PREFS_KEY_NOM = "nom"
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        //Pour sauvegarder le nom insérer au lancement de l'application
+        val inputNom = findViewById<EditText>(R.id.nameSpace)
+        val prefs = getSharedPreferences("prefs", Context.MODE_PRIVATE)
+        val nom = prefs.getString(PREFS_KEY_NOM, "")
+
+        inputNom.setText(nom)
+        inputNom.addTextChangedListener(object: TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+
+            override fun afterTextChanged(nom: Editable?) {
+                Log.d("MainActivity", "afterTextChanged : $nom")
+
+                prefs.edit().putString("nom", nom.toString()).apply()
+            }
+        })
     }
 }
